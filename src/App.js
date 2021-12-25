56ded59f313fe2dd6cf1eca3806c6e7838de434e
Andrew Smith <andrewsmith@Andrews-MacBook-Pro.local>
2021-12-25 14:44:57 -0500

    placeholder

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './katepic.jpeg';
+import bg from './bg.jpg';
+import './App.css';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Merry Christmas Katie!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
